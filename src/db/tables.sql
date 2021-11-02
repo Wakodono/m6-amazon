bf57d404dfb2ca5b9ab28156e789f61219b03107
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-11-02 22:21:25 +0000

    Update tables.sql

--- src/db/tables.sql
+++ src/db/tables.sql
@@ -1,5 +1,25 @@
 CREATE TABLE IF NOT EXISTS
 
-products ();
+products (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,    
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(280) NOT NULL,
+    brand VARCHAR(50) NOT NULL,
+    image_url VARCHAR(300),
+    price MONEY NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
 
-reviews ();
+CREATE TABLE IF NOT EXISTS
+
+reviews (
+  reviews(
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    comment VARCHAR(200) NOT NULL,
+    rate SMALLINT NOT NULL,
+    product_id VARCHAR(100) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+  );
+);
